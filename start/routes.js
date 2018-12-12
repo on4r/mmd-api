d24d1f40e09ded02d3b2ca665b7a7ed9cc505258
on4r <puqpetmaster@gmail.com>
2018-12-12 10:26:34 +0100

    Add user registration and login

--- start/routes.js
+++ start/routes.js
@@ -19,3 +19,8 @@ const Route = use('Route')
 Route.get('/', () => {
   return { greeting: 'Hello world in JSON' }
 })
+
+Route.group(() => {
+  Route.post('auth/register', 'UserController.register')
+  Route.post('auth/login', 'UserController.login')
+}).prefix('api');
